350fef61b73dd5298c6b169e041ede3a7340a345
Caroline Liu <caroline.liu111@gmail.com>
2019-12-17 21:33:22 -0800

    router and upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Upload from "./upload";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Upload from './upload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <Upload/>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path='/' component={Upload} />
+                        <Route path="/upload" component={Upload} exact={true} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;

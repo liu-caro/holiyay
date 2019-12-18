4df7f725df4666cfdbae5f0aff84c37478ed9246
Caroline Liu <caroline.liu111@gmail.com>
2019-12-17 21:50:13 -0800

    working gallery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Upload from './upload';
+import Gallery from './gallery'
 
 
 
@@ -12,6 +13,7 @@ class App extends Component {
                     <Switch>
                         <Route exact path='/' component={Upload} />
                         <Route path="/upload" component={Upload} exact={true} />
+                        <Route path="/gallery" component={Gallery} exact={true} />
                     </Switch>
                 </div>
             </Router>
---
+++ src/gallery.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import BackgroundSlideshow from 'react-background-slideshow';
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+
+
+class Gallery extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <BackgroundSlideshow images={[ img1, img2, img3 ]} animationDelay={900}/>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Gallery;

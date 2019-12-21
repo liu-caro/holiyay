6c3226bde9790b75cee43bdeee120acf2765aee1
Caroline Liu <caroline.liu111@gmail.com>
2019-12-21 11:40:40 -0800

    change gallery

--- src/gallery.js
+++ src/gallery.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
-import BackgroundSlideshow from 'react-background-slideshow';
 import img1 from './images/img1.jpg';
 import img2 from './images/img2.jpg';
 import img3 from './images/img3.jpg';
 import Firebase from "./firebase";
 import "firebase/storage";
-import Img from 'react-image';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
 
 
 class Gallery extends Component {
@@ -13,7 +13,6 @@ class Gallery extends Component {
         super(props);
         this.state = {
             imageObjs: []
-            // images: []
         };
     }
 
@@ -22,47 +21,21 @@ class Gallery extends Component {
         let imagesRef = Firebase.database().ref('images').orderByKey().limitToLast(100);
         imagesRef.on('child_added', snapshot => {
             /* Update React state when message is added at Firebase Database */
-            // let imageObj = { text: snapshot.val(), id: snapshot.key };
-            this.setState({ imageObjs: [snapshot.val().imageURL].concat(this.state.imageObjs) });
+            this.setState({ imageObjs: [{original: snapshot.val().imageURL}].concat(this.state.imageObjs) });
             console.log(this.state.imageObjs);
-            // this.setState({ images: [imageObj.text.URL].concat(this.state.images) });
         })
 
-    }
-
-    populateImages(){
-        let images = [];
-       // return this.state.imageObjs.map( imageRef =>
-       //     let images2 = <img src={imageRef.text.imageUrl || "https://via.placeholder.com/1280x850"} alt="holidayImage" />
-       //
-       //     );
     }
 
 
 
     render() {
-       let images = [];
-        // this.state.imageObjs.map( imageRef =>
-        //         images.push(<img src={imageRef} alt="holidayImage" />));
-
-        // images.push(<img src={imageRef || "https://via.placeholder.com/1280x850"} alt="holidayImage" />)
-        // let img = document.getElementById('myimg');
-        // img.src = url;
-
-
         return (
             <React.Fragment>
-
-                <BackgroundSlideshow images={this.state.imageObjs}/>
-                {/*{this.state.imageObjs.map( imageRef =>*/}
-                {/*images.push(<img src={imageRef} alt="holidayImage" />))}*/}
-                {/*{this.state.imageObjs.map( imageRef =>*/}
-                {/*    images.push(<img src={imageRef} alt="holidayImage" />))}*/}
-                {/*{console.log(images)}*/}
-                {/*<BackgroundSlideshow images={images}/>*/}
-            </React.Fragment>
-        );
-    }
+                 <ImageGallery items={this.state.imageObjs} />
+             </React.Fragment>
+         );
+     }
 }
 
 export default Gallery;
